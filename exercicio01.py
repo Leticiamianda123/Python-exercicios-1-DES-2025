3f20d7d9407510db2d2db5c19b25d32b0bcf3c2e
Leticiamianda123 <leticia.miranda.santos24@escola.pr.gov.br>
2025-06-03 11:58:31 +0000

    exercicio 01

--- exercicio01.py
+++ exercicio01.py
@@ -4,3 +4,19 @@
 # Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
 # Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
 
+
+
+
+dia_1 = int (input("dias para atividade 01 "))
+dia_2 = int(input("dias para atividade 02"))
+dia_3 = int (input("dia para atividade 03")
+             
+
+
+        if dia_01 ele <  or dia_02 < 0 or dia_03 <0:
+        print("erro números negativos ")
+
+        else:
+              soma = dia_01+ dia_02+dia_03
+        print(f"tempo total do projeto :{soma}dia")
+        
