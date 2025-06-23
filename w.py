ac327b11294e9230cb1bc362501cafd9045affae
Leticiamianda123 <leticia.miranda.santos24@escola.pr.gov.br>
2025-06-23 13:35:28 +0000

    exercicios terminados

---
+++ w.py
@@ -0,0 +1,10 @@
+import random
+
+input("pressione  o enter para laçar o dado")
+
+resultado = rodom randint(1,6)
+
+print (f"o dado rolou : {resultado }");
+
+if(resultado > 6 )
+   print("ual voce e fera ")
