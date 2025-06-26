1dbf89965e7ad19ba00da54a76d2a3cc291deaf5
Leticiamianda123 <leticia.miranda.santos24@escola.pr.gov.br>
2025-06-26 12:13:31 +0000

    atvidade feita

--- exercicio010.py
+++ exercicio010.py
@@ -3,3 +3,9 @@
 # Salário mensal acima de R$ 3.000,00
 # A parcela não pode ser maior que 35% do salário
 
+salario = float(input("digite o salario mesal de renata: $"))
+parcela = float (input("digite o valor da parcela dos funcionarios: R$"))
+
+if salario <= 3000:
+    print("finaciamentonegado,salario abaixo do minimo exigido")
+    
---
+++ iadade.py/idade .py
@@ -0,0 +1,7 @@
+
+nome = input("digite seu nome :")
+print("bem-vindo," + nome + "!")
+
+# convertendo entrada númerica 
+idade = int(input("digite sua idade:"))
+print(f"no próximo ano você terá {idade + 1 }ano.")
