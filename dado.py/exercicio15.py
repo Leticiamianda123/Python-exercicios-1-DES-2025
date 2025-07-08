cadf40b70846612d5259f3b8241107776d3a44df
Leticiamianda123 <leticia.miranda.santos24@escola.pr.gov.br>
2025-07-08 12:11:57 +0000

    feito

--- dado.py/exercicio15.py
+++ dado.py/exercicio15.py
@@ -1,3 +1,15 @@
 #Peça a idade do usuário e diga se ele pode se cadastrar em um site:
 
-#Permitido: 13 anos ou mais
+#permitido: 13 anos ou mais 
+
+idade = int(input("por favor , digite sua idade:"))
+
+if idade >=14:
+    print("voce pode se cadastrar no site. Bem vindo (a)!")
+else:
+    print("desculpe,voce nao pode se cadrastar no site ainda.A idade minima e de 13 anos.")
+    
+
+
+
+
